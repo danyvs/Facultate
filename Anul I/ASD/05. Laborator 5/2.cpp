05e2be781a09337477fa6dc2cf090b59ca40c81c
Daniel Sava <26028086+danyvs@users.noreply.github.com>
2018-11-03 11:35:52 +0200

    Create 2.cpp

---
+++ Anul I/ASD/05. Laborator 5/2.cpp
@@ -0,0 +1,102 @@
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int info;
+	Node* next;
+};
+
+/* Adaugarea unui nou nod la inceputul listei */
+void addHead(Node* &head, Node* &tail, int data) {
+	/* nu se trateaza cazul cand lista este vida, deaorece lista initiala nu
+	este vida si, pe tot parcursul executiei programului, nu se face nicio stergere
+	in lista */
+	Node* temp = new Node;
+	temp->info = data;
+	temp->next = head;
+	head = temp;
+}
+
+/* Adaugarea unui nou nod la finalul listei */
+void addTail(Node* &head, Node* &tail, int data) {
+	/* nu se trateaza cazul cand lista este vida, deaorece lista initiala nu
+	este vida si, pe tot parcursul executiei programului, nu se face nicio stergere
+	in lista */
+	Node* temp = new Node;
+	temp->info = data;
+	temp->next = NULL;
+	tail->next = temp;
+	tail = temp;
+}
+
+/* Returneaza un pointer catre valoarea dupa care trebuie inserata valoarea curenta*/
+Node* positionToInsert(Node* head, Node* tail, int data) {
+	Node* it = head;
+	while (it->next && data < it->next->info)
+		it = it->next;
+	return it;
+}
+
+/* Afisarea listei */
+void printList(Node* head, Node* tail) {
+	for (Node* it = head; it; it = it->next)
+		cout << it->info << " ";
+	cout << "\n";
+}
+
+/* Crearea unei liste sortate */
+void createSortedList(Node* &head, Node* &tail) {
+	int n;
+	cout << "Dati numarul de elemente al listei: ";
+	cin >> n;
+
+	// initializarea listei
+	head = tail = NULL; 
+
+	if (n <= 0) {
+		cout << "Numarul de elemente al listei trebuie sa fie >= 0\n";
+		exit(EXIT_SUCCESS);
+	}
+
+	cout << "Dati elementele listei: ";
+
+	/* primul nod se trateaza separat, deoarece nu poate fi comparat cu
+	niciun alt nod */
+	int data;
+	cin >> data;
+	head = tail = new Node;
+	head->info = data;
+	head->next = NULL;
+	printList(head, tail);
+
+	for (int i = 1; i < n; ++i) {
+		cin >> data;
+		if (data <= head->info) {
+			// valoarea curenta este mai mica (sau egala) decat cea din primul nod al listei
+			addHead(head, tail, data);
+		}
+		else {
+			if (data > tail->info) {
+				// informatia curenta este mai mare decat cea din primul nod al listei
+				addTail(head, tail, data);
+			}
+			else {
+				// informatia curenta trebuie inserata intre doua noduri deja existente
+				Node* pos = positionToInsert(head, tail, data);
+				Node* temp = new Node;
+				temp->info = data;
+				temp->next = pos->next;
+				pos->next = temp;
+			}
+		}
+		printList(head, tail);
+	}
+}
+
+int main() {
+	Node *head, *tail;
+	createSortedList(head, tail);
+	return 0;
+}
